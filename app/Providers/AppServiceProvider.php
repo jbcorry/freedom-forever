c18e056e363dc940e9143c139281e0d653046a33
Jacob Corry <jcorry@slcm-jcorry.lan>
2020-10-26 21:42:27 -0600

    add a log to check if conditional is being reached

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         if (!app()->isLocal()) {
+            Log::info('production!!!!!');
             URL::forceScheme('https');
         }
     }
